29f09d9e129ab967736bb1137a85af3b7e545d18
김현지 <khyunjiee@gmail.com>
2021-08-28 00:03:35 +0900

    update: 뱃지 클래스 네이밍 수정

--- src/class/Badge.ts
+++ src/class/Badge.ts
@@ -3,18 +3,18 @@ class Badge {
   // 아이디, 이름, 획득방법, AWS S3 이미지 URL
   private id: number;
   private name: string;
-  private howObtain: string;
+  private information: string;
   private imageURL: string;
 
   constructor(id: number, name: string, howObtain: string, imageURL: string) {
     this.id = id;
     this.name = name;
-    this.howObtain = howObtain;
+    this.information = howObtain;
     this.imageURL = imageURL;
   }
 
   getId(): number {return this.id;}
   getName(): string {return this.name;}
-  getHowObtain(): string {return this.howObtain;}
+  getHowObtain(): string {return this.information;}
   getImageURL(): string {return this.imageURL;}
 }
