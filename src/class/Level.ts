936c5493e5abca5ea20dc6eafe952b5ba0122beb
김현지 <khyunjiee@gmail.com>
2021-08-27 21:32:17 +0900

    update: 레벨 클래스 수정

--- src/class/Level.ts
+++ src/class/Level.ts
@@ -1,10 +1,19 @@
 class Level {
   private level: number;
   private fullHappy: number;
+  private characterType?: number;
 
   constructor(level: number, fullHappy: number) {
     this.level = level;
     this.fullHappy = fullHappy;
+  }
+
+  setCharacterType(type: number) {
+    this.characterType = type;
+  }
+
+  getCharacterType(): number {
+    return this.characterType;
   }
 
   getLevel(): number {
