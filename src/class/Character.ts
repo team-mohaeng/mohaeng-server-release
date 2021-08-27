f246cf47aff8275095359e363eb4f1f1b431609b
김현지 <khyunjiee@gmail.com>
2021-08-27 21:13:04 +0900

    create: 캐릭터, 캐릭터 카드, 스킨 클래스 정의

---
+++ src/class/Character.ts
@@ -0,0 +1,22 @@
+class Character {
+  private type: number;
+  private cards: [CharacterCard];
+
+  constructor(type: number) {
+    this.type = type;
+  }
+
+  addCharacterCard(id: number, imageURL: string): void {
+    const card = new CharacterCard(id, imageURL);
+    this.cards.push(card);
+  }
+
+  getType(): number {
+    return this.type;
+  }
+
+  getCards(): [CharacterCard] {
+    return this.cards;
+  }
+
+}
---
+++ src/class/CharacterCard.ts
@@ -0,0 +1,18 @@
+class CharacterCard {
+  private id: number;
+  private imageURL: string;
+
+  constructor(id: number, imageURL: string) {
+    this.id = id;
+    this.imageURL = imageURL;
+  }
+
+  getId(): number {
+    return this.id;
+  }
+
+  getImageURL(): string {
+    return this.imageURL;
+  }
+  
+}
