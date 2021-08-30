dced58f2cb0b9f0dae6b11ead0da7c4ee81924c8
김기연 <rusl0325@sookmyung.ac.kr>
2021-08-30 17:25:42 +0900

    feat: 닉네임 변경 api 작성

---
+++ src/api/profile.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import { ChangeNicknameRequestDTO } from "../dto/MyPage/nickname/request/ChangeNicknameRequestDto";
+import profileService from "../service/profileService";
+
+const router = express.Router();
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const nickname = req.body.nickname;
+  const requestDTO: ChangeNicknameRequestDTO = {
+    nickname: nickname
+  };
+  const result = await profileService.changeNickname(id, requestDTO);
+  res.status(result.status).json(result);
+})
+
+module.exports = router;
