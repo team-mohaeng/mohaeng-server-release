7a9829f9b14642ccbe896027d21b1a3d472547f8
김기연 <rusl0325@sookmyung.ac.kr>
2021-09-05 15:44:55 +0900

    feat: 에러 추가

--- src/errors.ts
+++ src/errors.ts
@@ -1,21 +1,37 @@
 import { IFail } from "./interfaces/IFail";
+import { SERVER_ERROR_MESSAGE } from "./constant";
+
+export const serverError: IFail = {
+  status: 500,
+  message: SERVER_ERROR_MESSAGE
+}
 
 export const notExistUser: IFail = {
   status: 401,
   message: "유저가 존재하지 않습니다.",
 };
 
-export const characterLengthCheck: IFail = {
-  status: 400,
-  message: "닉네임은 1~6글자 이내로 작성해주세요.",
+export const nicknameLengthCheck: IFail = {
+  status: 412,
+  message: "닉네임은 1~6글자 이내로 작성해주세요",
 };
 
 export const sameNickname: IFail = {
-  status: 403,
-  message: "기존 닉네임과 다른 닉네임으로 설정해주세요.",
+  status: 412,
+  message: "기존 닉네임과 다른 닉네임으로 설정해주세요",
 };
 
-export const nicknameDuplication: IFail = {
-  status: 403,
-  message: "이미 사용중인 닉네임입니다.",
+export const alreadyExistNickname: IFail = {
+  status: 412,
+  message: "이미 사용중인 닉네임입니다",
 };
+
+export const alreadyExistEmail: IFail = {
+  status: 412,
+  message: "중복된 이메일입니다"
+}
+
+export const notExistToken: IFail = {
+  status: 400,
+  message: "토큰 생성에 실패하였습니다."
+}
