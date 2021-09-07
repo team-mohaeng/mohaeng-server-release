dca5420898e04b69501460d728ff1384807d1d42
김기연 <rusl0325@sookmyung.ac.kr>
2021-09-08 04:06:08 +0900

    feat: 닉네임 변경 service 수정

--- src/service/profileService.ts
+++ src/service/profileService.ts
@@ -3,9 +3,10 @@ import { ChangeNicknameRequestDTO } from "../dto/MyPage/nickname/request/ChangeN
 import { ChangeNicknameResponseDTO } from "../dto/MyPage/nickname/response/ChangeNicknameResponseDto";
 import { SERVER_ERROR_MESSAGE } from "../constant";
 import { IFail } from "../interfaces/IFail";
+import { notExistUser, nicknameLengthCheck, sameNickname, alreadyExistNickname } from "../errors";
 
 export default {
-  changeNickname: async (id: number, dto: ChangeNicknameRequestDTO) => {
+  changeNickname: async (id: string, dto: ChangeNicknameRequestDTO) => {
     try{
       const { nickname } = dto;
       const user = await User.findOne({ attributes: ['nickname'], where: {id: id} });
@@ -15,7 +16,7 @@ export default {
       }
 
       if ( nickname.length > 6 || nickname.length ==0 ) {
-        return characterLengthCheck;
+        return nicknameLengthCheck;
       }
     
       if (nickname == user.nickname) {
@@ -24,7 +25,7 @@ export default {
       
       const hasNickname = await User.findOne({ attributes: ['nickname'], where: {nickname: nickname} });
       if (hasNickname) {
-        return nicknameDuplication;
+        return alreadyExistNickname;
       }
       
       await User.update({
