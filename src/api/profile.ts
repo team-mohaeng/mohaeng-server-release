69fac31d79cf8068d5e0eba67355bc6de18198f1
김기연 <rusl0325@sookmyung.ac.kr>
2021-09-08 04:05:42 +0900

    feat: 프로필 api jwt 토큰 인증해서 완성

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { ChangeNicknameRequestDTO } from "../dto/MyPage/nickname/request/ChangeNicknameRequestDto";
 import profileService from "../service/profileService";
+import auth from "../middleware/auth";
 
 const router = express.Router();
 
-router.put("/:id", async (req, res) => {
-  const id = req.params.id;
+router.put("/", auth, async (req, res) => {
+  console.log(req.body.user);
   const nickname = req.body.nickname;
   const requestDTO: ChangeNicknameRequestDTO = {
     nickname: nickname
   };
-  const result = await profileService.changeNickname(id, requestDTO);
+  const result = await profileService.changeNickname(req.body.user.id, requestDTO);
   res.status(result.status).json(result);
 })
 
