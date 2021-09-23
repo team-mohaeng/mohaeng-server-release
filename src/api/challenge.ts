9569e8b64e16860c9688c21eae7d38db40392d5b
김현지 <khyunjiee@gmail.com>
2021-09-23 21:21:11 +0900

    update: 챌린지 인증하기 API 완성

--- src/api/challenge.ts
+++ src/api/challenge.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import auth from "../middleware/auth";
 import challengeService from '../service/challengeService';
+import courseService from '../service/courseService';
 
 const router = express.Router();
 
 router.get("/", auth, async (req, res) => {
   const result = await challengeService.today(req.body.user.id);
+  res.status(result.status).json(result);
+});
+
+router.put("/:courseId/:challengeId", auth, async (req, res) => {
+  const result = await challengeService.certification(req.body.user.id, req.params.courseId, req.params.challengeId);
   res.status(result.status).json(result);
 });
 
