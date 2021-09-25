e7f69784c86b36be4419129597467322f6eb42e2
김현지 <khyunjiee@gmail.com>
2021-09-26 02:09:55 +0900

    create: 오늘의 챌린지 조회 apidoc 작성

---
+++ src/api/docs/challenge.ts
@@ -0,0 +1,144 @@
+/**
+ * @api {get} /api/today 오늘의 챌린지 조회
+ * 
+ * @apiVersion 1.0.0
+ * @apiName Today
+ * @apiGroup 챌린지
+ * 
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ *  "Content-Type": "application/json",
+ *  "Bearer": "{jwt}"
+ * }
+ * 
+ * @apiSuccess {Boolean} isComplete 챌린지 인증 상태
+ * @apiSuccess {Boolean} isPenalty 패널티 적용 여부
+ * @apiSuccess {String} mainCharacterImg 메인 뷰 상단 말풍선 속 캐릭터 이미지
+ * @apiSuccess {String} popupCharacterImg 챌린지 인증 팝업 캐릭터 이미지
+ * @apiSuccess {Object} course 현재 진행 중인 코스
+ * @apiSuccess {Number} id 코스 아이디
+ * @apiSuccess {Number} situation 코스 진행 상태 (1: 진행 중, 2: 완료)
+ * @apiSuccess {Number} property 코스 속성 (1 ~ 7 -> 자세한 목록은 추후 슬랙에 공지)
+ * @apiSuccess {String} title 코스 제목
+ * @apiSuccess {Number} totalDays 코스 총 날짜 수
+ * @apiSuccess {Number} currentDay 현재 진행 중인 챌린지 날짜
+ * @apiSuccess {String} year 코스를 완료한 년도
+ * @apiSuccess {String} month 코스를 완료한 월
+ * @apiSuccess {String} date 코스를 완료한 일
+ * @apiSuccess {Object[]} challenges 코스에 포함되어있는 챌린지들
+ * @apiSuccess {Number} day 챌린지 날짜
+ * @apiSuccess {Number} situation 코스 진행 상태 (0: 진행 전, 1: 진행 중, 2: 완료)
+ * @apiSuccess {String} title 챌린지 제목
+ * @apiSuccess {Number} happy 챌린지 성공 시 제공되는 해피지수
+ * @apiSuccess {String} beforeMent 챌린지 수행 전 멘트
+ * @apiSuccess {String} afterMent 챌린지 수행 완료 멘트
+ * @apiSuccess {String} year 챌린지를 완료한 년도
+ * @apiSuccess {String} month 챌린지를 완료한 월
+ * @apiSuccess {String} date 챌린지를 완료한 일
+ * @apiSuccess {String[]} badges 해당 챌린지 완료 시 받을 수 있는 뱃지 이름 목록
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ * 200 챌린지 진행 중
+ * {
+ *  "status": 200,
+ *  "data": {
+ *    "isComplete": false,
+ *    "isPenalty": false,
+ *    "mainCharacterImg": "url",
+ *    "popupCharacterImg": "url",
+ *    "course": {
+ *      "id": 1,
+ *      "situation": 1,
+ *      "property": 1,
+ *      "title": "초보 사진가",
+ *      "totalDays": 7,
+ *      "currentDay": 1,
+ *      "year": "",
+ *      "month": "",
+ *      "date": "",
+ *      "challenges": [
+ *        {
+ *          "day": 1,
+ *          "situation": 1,
+ *          "title": "하늘 사진 찍기"
+ *          "happy": 12,
+ *          "beforeMent": "난 몽글몽글한 구름들을 보면 기분이 좋아지더라~ 오늘 너희 동네의 하늘은 어때?",
+ *          "afterMent": "엇, 그 구름 약간 날 닮은 것 같은데!? 일부러 이런 구름 찍어온거야? 너~",
+ *          "year": "",
+ *          "month": "",
+ *          "date": "",
+ *          "badges": [
+ *            "성장한 챌린저",
+ *            "모행 홀릭 챌린저"
+ *          ]
+ *        },
+ *        // ...
+ *      ]
+ *    }
+ *  }
+ * }
+ * 
+ * 200 챌린지 진행 완료해서 코스 완료
+ * {
+ *  "status": 200,
+ *  "data": {
+ *    "isComplete": true,
+ *    "isPenalty": false,
+ *    "mainCharacterImg": "url",
+ *    "popupCharacterImg": "url",
+ *    "course": {
+ *      "id": 1,
+ *      "situation": 2,
+ *      "property": 1,
+ *      "title": "초보 사진가",
+ *      "totalDays": 7,
+ *      "currentDay": 7,
+ *      "year": "2021",
+ *      "month": "09",
+ *      "date": "26",
+ *      "challenges": [
+ *        {
+ *          "day": 1,
+ *          "situation": 2,
+ *          "title": "하늘 사진 찍기"
+ *          "happy": 12,
+ *          "beforeMent": "난 몽글몽글한 구름들을 보면 기분이 좋아지더라~ 오늘 너희 동네의 하늘은 어때?",
+ *          "afterMent": "엇, 그 구름 약간 날 닮은 것 같은데!? 일부러 이런 구름 찍어온거야? 너~",
+ *          "year": "2021",
+ *          "month": "09",
+ *          "date": "20",
+ *          "badges": []
+ *        },
+ *        // ...
+ *        {
+ *          "day": 7,
+ *          "situation": 2,
+ *          "title": "하늘 사진 찍기"
+ *          "happy": 12,
+ *          "beforeMent": "난 몽글몽글한 구름들을 보면 기분이 좋아지더라~ 오늘 너희 동네의 하늘은 어때?",
+ *          "afterMent": "엇, 그 구름 약간 날 닮은 것 같은데!? 일부러 이런 구름 찍어온거야? 너~",
+ *          "year": "2021",
+ *          "month": "09",
+ *          "date": "26",
+ *          "badges": [
+ *            "건강 코스 3개"
+ *          ]
+ *        },
+ *      ]
+ *    }
+ *  }
+ * }
+ * 
+ * @apiErrorExample Error-Response:
+ * 401 존재하지 않는 유저
+ * {
+ *  "status": 401,
+ *  "message": "유저가 존재하지 않습니다."
+ * }
+ * 
+ * 404 진행 중인 코스가 없는 경우
+ * {
+ *  "status": 404,
+ *  "message": "진행 중인 코스가 없습니다."
+ * }
+ */
