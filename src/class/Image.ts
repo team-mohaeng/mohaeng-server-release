6d643bd0ac21cdc7051d93d36effb358206f346a
김현지 <khyunjiee@gmail.com>
2021-10-12 14:16:50 +0900

    update: Image 클래스에 로티 url 필드 추가

--- src/class/Image.ts
+++ src/class/Image.ts
@@ -1,15 +1,18 @@
 class Image {
   private characterType: number;
   private cardId: number;
+  private lottieURL: string;
   private imageURLs: [string];
 
-  constructor(characterType: number, cardId: number, imageURLs: [string])  {
+  constructor(characterType: number, cardId: number, lottieURL: string, imageURLs: [string])  {
     this.characterType = characterType;
     this.cardId = cardId;
+    this.lottieURL = lottieURL;
     this.imageURLs = imageURLs;
   }
 
   getCharacterType(): number {return this.characterType;}
-  getCardId(): number {return this.cardId;}
+  getCardId(): number { return this.cardId; }
+  getLottieURL(): string { return this.lottieURL; }
   getImageURLs(): [string] {return this.imageURLs;}
 }
