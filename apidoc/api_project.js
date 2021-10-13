f3c2d95bda645fec002d36a69050ba0e2e35702a
김현지 <khyunjiee@gmail.com>
2021-10-13 12:45:47 +0900

    docs: apidoc 작성

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-12T11:29:56.376Z",
+    "time": "2021-10-13T03:44:56.670Z",
     "url": "https://apidocjs.com",
-    "version": "0.29.0"
+    "version": "0.28.1"
   }
 });
