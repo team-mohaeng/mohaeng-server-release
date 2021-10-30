e239b2b90e501392cb30f57ebbec2eabc7f2e01e
김기연 <rusl0325@sookmyung.ac.kr>
2021-10-30 23:10:07 +0900

    feat: 애플 로그인 미들웨어 오류 해결

--- src/middleware/apple.ts
+++ src/middleware/apple.ts
@@ -1,8 +1,48 @@
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import { invalidToken, notExistToken } from "../errors";
+const NodeRSA = require('node-rsa');
 
 //Get public key
+export default async(req, res, next) => {
+  const idToken = req.header("idToken");
+  if (!idToken) {
+    res.status(notExistToken.status).json(notExistToken);
+  }
+  const result = await axios.request({
+    method: "GET",
+    url: "https://appleid.apple.com/auth/keys",
+  })
+  const data = result.data;
+  const jwtClaims = verifyIdToken(data, idToken);
+  jwtClaims
+  .then((token) => { 
+    const sub = token.sub
+    req.body.sub = sub;
+    next();
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(invalidToken.status).json(invalidToken);
+  })
+};
+//Decryption of id'u token by public key and rs256 algorithm
+async function verifyIdToken(data, idToken) { //id_token, client_id
+    const decodedToken = jwt.decode(idToken, { complete: true });
+    const keys = data.keys;
+    const key = keys.find(k => k.kid === decodedToken.header.kid);
+    const publicKey = new NodeRSA();
+    publicKey.importKey({ n: Buffer.from(key.n, 'base64'), e: Buffer.from(key.e, 'base64') }, 'components-public');
+    const applePublicKey = publicKey.exportKey(['public'])
+    const jwtClaims = jwt.verify(idToken, applePublicKey, { algorithms: ['RS256'] });
+    return jwtClaims;
+};
+
+
+/*
+import jwt from "jsonwebtoken";
+import { invalidToken, notExistToken } from "../errors";
+
 export default async(req, res, next) => {
   const idToken = req.header("idToken");
   if (!idToken) {
@@ -18,3 +58,4 @@ export default async(req, res, next) => {
     res.status(invalidToken.status).json(invalidToken);
   }
 };
+*/
