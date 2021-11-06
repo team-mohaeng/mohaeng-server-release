8302c88e577a5b2f407f67c07ec499c69fa940ac
김현지 <khyunjiee@gmail.com>
2021-11-06 11:51:56 +0900

    update: skin dummy

--- src/dummy/Skin.ts
+++ src/dummy/Skin.ts
@@ -1,14 +1,5 @@
 import { Skin } from "../class/Skin";
 
-export const skins = [
-  new Skin(64, ""),
-  new Skin(65, ""),
-  new Skin(66, ""),
-  new Skin(67, ""),
-  new Skin(68, ""),
-  new Skin(69, ""),
-]
-
 export const iosSkins = [
   new Skin(64, "https://sopt28-test.s3.ap-northeast-2.amazonaws.com/ios1.png"),
   new Skin(65, "https://sopt28-test.s3.ap-northeast-2.amazonaws.com/ios2.png"),
