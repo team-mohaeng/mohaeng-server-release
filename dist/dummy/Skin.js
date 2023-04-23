919d38944594ec0896c644643727cef50284ed1c
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-04-23 22:30:54 +0900

    chore: tsc 컴파일

--- dist/dummy/Skin.js
+++ dist/dummy/Skin.js
@@ -3,19 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.aosSkins = exports.iosSkins = void 0;
 const Skin_1 = require("../class/Skin");
 exports.iosSkins = [
-    new Skin_1.Skin(64, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/1.png"),
-    new Skin_1.Skin(65, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/2.png"),
-    new Skin_1.Skin(66, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/3.png"),
-    new Skin_1.Skin(67, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/4.png"),
-    new Skin_1.Skin(68, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/5.png"),
-    new Skin_1.Skin(69, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/6.png"),
+    new Skin_1.Skin(64, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/1.png"),
+    new Skin_1.Skin(65, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/2.png"),
+    new Skin_1.Skin(66, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/3.png"),
+    new Skin_1.Skin(67, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/4.png"),
+    new Skin_1.Skin(68, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/5.png"),
+    new Skin_1.Skin(69, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/6.png"),
 ];
 exports.aosSkins = [
-    new Skin_1.Skin(64, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/1.png"),
-    new Skin_1.Skin(65, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/2.png"),
-    new Skin_1.Skin(66, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/3.png"),
-    new Skin_1.Skin(67, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/4.png"),
-    new Skin_1.Skin(68, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/5.png"),
-    new Skin_1.Skin(69, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/6.png"),
+    new Skin_1.Skin(64, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/1.png"),
+    new Skin_1.Skin(65, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/2.png"),
+    new Skin_1.Skin(66, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/3.png"),
+    new Skin_1.Skin(67, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/4.png"),
+    new Skin_1.Skin(68, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/5.png"),
+    new Skin_1.Skin(69, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/6.png"),
 ];
 //# sourceMappingURL=Skin.js.map
--- src/dummy/Skin.ts
+++ src/dummy/Skin.ts
@@ -1,20 +1,20 @@
 import { Skin } from "../class/Skin";
 
 export const iosSkins = [
-  new Skin(64, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/1.png"),
-  new Skin(65, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/2.png"),
-  new Skin(66, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/3.png"),
-  new Skin(67, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/4.png"),
-  new Skin(68, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/5.png"),
-  new Skin(69, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/6.png"),
+  new Skin(64, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/1.png"),
+  new Skin(65, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/2.png"),
+  new Skin(66, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/3.png"),
+  new Skin(67, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/4.png"),
+  new Skin(68, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/5.png"),
+  new Skin(69, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/iOS/6.png"),
 ]
 
 export const aosSkins = [
-  new Skin(64, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/1.png"),
-  new Skin(65, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/2.png"),
-  new Skin(66, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/3.png"),
-  new Skin(67, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/4.png"),
-  new Skin(68, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/5.png"),
-  new Skin(69, "https://mohaengimage.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/6.png"),
+  new Skin(64, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/1.png"),
+  new Skin(65, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/2.png"),
+  new Skin(66, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/3.png"),
+  new Skin(67, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/4.png"),
+  new Skin(68, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/5.png"),
+  new Skin(69, "https://mohaengimg.s3.ap-northeast-2.amazonaws.com/styles/skins/AOS/6.png"),
 ]
 
